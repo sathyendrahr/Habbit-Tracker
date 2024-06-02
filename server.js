5c65ba37f6c3e9669cfd17ebd9b4e14eca731043
sathyendrahr <56423699+sathyendrahr@users.noreply.github.com>
2024-06-03 03:54:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { connectToDatabase } from "./config/dbconfig.js";
 
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}`);
